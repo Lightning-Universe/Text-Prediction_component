40aecdb313c376c737d9344f975cc14ca0c59dc4
Justus Schock <justus.schock@posteo.de>
2022-12-20 17:30:48 +0100

    rename

--- app.py
+++ app.py
@@ -4,79 +4,57 @@
 
 
 import lightning as L
-import os, torch, deepspeed
-from lightning_mingpt import models, data
+import os, torch
+from lightning_mingpt import models
 from lit_llms.tensorboard import DriveTensorBoardLogger, MultiNodeLightningTrainerWithTensorboard
 
-from lai_charpred import default_callbacks, gpt_10b, gpt_20b, gpt_45b
+from lai_textpred import default_callbacks, gpt_10b, WordDataset
 
 
-class MyDeepspeedGPT(models.DeepSpeedGPT):
-    # def __init__(self, *args, activation_checkpointing: bool = False, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     if activation_checkpointing:
-    #         self.forward = deepspeed.checkpointing.checkpoint(self.forward)
-    def configure_optimizers(self):
-        return self.mingpt.configure_optimizers(self.mingpt_trainer_config)
-
-
-class CharacterPrediction(L.LightningWork):
+class WordPrediction(L.LightningWork):
     def __init__(self, *args, tb_drive, **kwargs):
         super().__init__(*args, **kwargs)
         self.tensorboard_drive = tb_drive
 
     def run(self):
+        # -------------------
+        # CONFIGURE YOUR DATA
+        # -------------------
         with open(os.path.expanduser("~/data/shakespeare/input.txt")) as f:
             text = f.read()
-        dataset = data.CharDataset(text, 50)
-        train_dset, val_dset = torch.utils.data.random_split(
-            dataset, [0.7, 0.3], generator=torch.Generator().manual_seed(42)
-        )
+        train_dataset = WordDataset(text, 5)
         train_loader = torch.utils.data.DataLoader(
-            train_dset, batch_size=1, num_workers=4, shuffle=True
-        )
-        val_loader = torch.utils.data.DataLoader(
-            val_dset, batch_size=1, num_workers=4, shuffle=False
+            train_dataset, batch_size=1, num_workers=4, shuffle=True
         )
 
-        model = MyDeepspeedGPT(
-            vocab_size=dataset.vocab_size,
-            block_size=int(dataset.block_size),
-            model_type=None,
-            **gpt_20b,
-            learning_rate=3e-4,
-            embd_pdrop=0.1,
-            resid_pdrop=0.1,
-            attn_pdrop=0.1,
-            weight_decay=0.1,
-            betas=(0.9, 0.95),
+        # --------------------
+        # CONFIGURE YOUR MODEL
+        # --------------------
+        model = models.FSDPGPT(
+            vocab_size=train_dataset.vocab_size, block_size=int(train_dataset.block_size),
+            model_type=None, **gpt_10b,
         )
 
+        # feature of PyTorch 2.0 for speedup
         if hasattr(torch, "compile"):
             model = torch.compile(model)
 
-        # strategy = L.pytorch.strategies.DeepSpeedStrategy(stage=3,
-        #     offload_optimizer=True,
-        #     offload_parameters=True,
-        #     partition_activations=True,
-        #     cpu_checkpointing= False
-        # )
 
+        # -------------------
+        # RUN YOUR FINETUNING
+        # -------------------
         trainer = L.Trainer(
-            precision=16,
-            # strategy='ddp',
-            strategy="deepspeed_stage_3_offload",
-            # strategy=strategy,
-            callbacks=default_callbacks(),
+            max_epochs=2, limit_train_batches=250,
+            precision=16, strategy="deepspeed_stage_3_offload",
+            callbacks=default_callbacks(), log_every_n_steps=5,
             logger=DriveTensorBoardLogger(save_dir=".", drive=self.tensorboard_drive),
-            log_every_n_steps=5,
         )
-        trainer.fit(model, train_loader, val_loader)
+        trainer.fit(model, train_loader)
 
 
 app = L.LightningApp(
     MultiNodeLightningTrainerWithTensorboard(
-        CharacterPrediction,
+        WordPrediction,
         num_nodes=3,
         cloud_compute=L.CloudCompute("gpu-fast-multi"),
     )
--- lai_charpred/__init__.py
+++
@@ -1,5 +0,0 @@
-from lai_charpred.callbacks import default_callbacks
-from lai_charpred.configs import *
-from lai_charpred.configs import __all__ as __all_configs
-
-__all__ = ["default_callbacks", "DriveTensorBoardLogger", "Main"] + __all_configs
