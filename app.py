50f467e155607d179aec63d11f0ccf198c439ca3
Justus Schock <justus.schock@posteo.de>
2022-12-22 15:56:43 +0100

    merge main

--- app.py
+++ app.py
@@ -5,36 +5,44 @@
 
 import lightning as L
 import os, torch
-from lightning_mingpt import models, data
+from lightning_mingpt import models
 from lit_llms.tensorboard import DriveTensorBoardLogger, MultiNodeLightningTrainerWithTensorboard
 
-from lai_charpred import default_callbacks, error_if_local, gpt_20b
+from lai_textpred import default_callbacks, gpt_10b, WordDataset
 
 
-class CharacterPrediction(L.LightningWork):
+class WordPrediction(L.LightningWork):
     def __init__(self, *args, tb_drive, **kwargs):
         super().__init__(*args, **kwargs)
         self.tensorboard_drive = tb_drive
 
     def run(self):
         error_if_local()
+
+        # -------------------
+        # CONFIGURE YOUR DATA
+        # -------------------
         with open(os.path.expanduser("~/data/shakespeare/input.txt")) as f:
             text = f.read()
-        train_dataset = data.CharDataset(text, 50)
-
+        train_dataset = WordDataset(text, 5)
         train_loader = torch.utils.data.DataLoader(
             train_dataset, batch_size=1, num_workers=4, shuffle=True
         )
 
+        # --------------------
+        # CONFIGURE YOUR MODEL
+        # --------------------
         model = models.FSDPGPT(
             vocab_size=train_dataset.vocab_size, block_size=int(train_dataset.block_size),
-            model_type=None, **gpt_20b,
+            model_type=None, **gpt_10b,
         )
 
-        if hasattr(torch, "compile"):
-            model = torch.compile(model)
 
+        # -------------------
+        # RUN YOUR FINETUNING
+        # -------------------
         trainer = L.Trainer(
+            max_epochs=2, limit_train_batches=250,
             precision=16, strategy="deepspeed_stage_3_offload",
             callbacks=default_callbacks(), log_every_n_steps=5,
             logger=DriveTensorBoardLogger(save_dir=".", drive=self.tensorboard_drive),
@@ -44,7 +52,7 @@ class CharacterPrediction(L.LightningWork):
 
 app = L.LightningApp(
     MultiNodeLightningTrainerWithTensorboard(
-        CharacterPrediction,
+        WordPrediction,
         num_nodes=3,
         cloud_compute=L.CloudCompute("gpu-fast-multi"),
     )
